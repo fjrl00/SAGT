a927f66be3542257079bd555fb510dc0425d4610
fjrl00 <frl711m@uma.es>
2025-10-30 21:38:16 +0100

    First foray into FlaUI

---
+++ PFC-SAGT v1.0.215/GUI_TG/Tests/IntegrationTests.cs
@@ -0,0 +1,81 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Tools;
+using FlaUI.Core.WindowsAPI;
+using FlaUI.UIA3;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class IntegrationTests
+    {
+        /* FlaUI kinks:
+         * - A menuStrip's 'Name' is its text instead of its name. Its AutomationId is its 'Name' however.
+         * - ToolStripMenuItem's do not have an AutomationId, and their 'Name' is also set to their text.
+         *      Furthermore, both their Name and Texts are language sensitive.
+         * - Buttons do seemingly not have Text? Their AutomationId is their name and their Name is their Text
+         * - These are seemingly WinForms problems and would be solved by migrating to WPF.
+         * - Some things seemingly can't really be debugged step by step no matter what we do,
+         *      abrir archivo de datos -> .Click() will not actually open the hidden menu unless the window was focused,
+         *      and operating the debugger AFAIK requires Visual Studio's tab to be focused.
+         *      Meanwhile, .Invoke opens the hidden menu for only a small blip of time, not available when debugging either.
+         */
+
+        /*
+         * Requirements to run:
+         * - Spanish language
+         * - Ejemplo1.sagt must exist in workspace
+         * - Don't have tabs capable of blocking our app on the foreground
+         */ 
+        [Fact]
+        public void BasicLoadingTest()
+        {
+            string AppPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\GUI_TG\bin\Debug\GUI_TG.exe"));
+
+            // These three seemingly do not work. No easy way to input these paths into the OpenFileDialog. 
+            //  trying to 'hack' the text in with fileBox.Text = text results in weird unintended events being triggered
+            //  Trying to use .Enter(text) somehow fails when attempting to enter a full "C:\..." path, selecting some file in the workbench instead
+            //string datosFile = Path.Combine(AppContext.BaseDirectory, "TestData", "datos.sagt");
+            //string mediasFile = Path.Combine(AppContext.BaseDirectory, "TestData", "medias.sagt");
+            //string ssqqFile = Path.Combine(AppContext.BaseDirectory, "TestData", "ssqq.sagt");
+
+            var app = FlaUI.Core.Application.Launch(AppPath);
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+
+                var openFileData = window.FindFirstDescendant(cf => cf.ByText("Abrir archivo de datos"))?.AsMenuItem();
+                openFileData.Click();
+
+                var openLocal = openFileData.FindFirstDescendant(cf => cf.ByText("Abrir"))?.AsMenuItem();
+                openLocal.Click();  // Using .Invoke() here will cause the test to freeze (problems regarding the new Form that's about to open)
+
+                var openDialog = Retry.WhileNull(   //need to use some sort of async method since it takes a little bit to load
+                    () => window.ModalWindows.FirstOrDefault(w => w.Title.Contains("Open")),
+                    TimeSpan.FromSeconds(3)
+                ).Result;
+
+                var fileBox = openDialog.FindFirstDescendant(cf => cf.ByControlType(FlaUI.Core.Definitions.ControlType.Edit)).AsTextBox();
+                fileBox.Enter("Ejemplo1.sagt");
+
+                // This is the bottom right 'Open' button. Trying to find it through text doesn't work
+                // since it will be confused with the dropdown button of the file name field.
+                // Also must use FindFirstChild and not FindFirstDescendant since the second file
+                // shown in the file explorer will also have AutomationId 1.
+                // Information obtained thanks to Inspect.exe from the Windows SDK.
+                var openButton = openDialog.FindFirstChild(cf => cf.ByAutomationId("1")).AsButton();   
+                openButton.Click();
+
+                // Step 4: Verify data loaded in textbox
+                var tbDescription = window.FindFirstDescendant(cf => cf.ByAutomationId("tbDescription")).AsTextBox();
+                Assert.Equal("Ejemplo1", tbDescription.Text);
+            }
+        }
+    }
+}
--- PFC-SAGT v1.0.215/GUI_TG/Tests/ReadingAndWritingTests.cs
+++ PFC-SAGT v1.0.215/GUI_TG/Tests/ReadingAndWritingTests.cs
@@ -20,12 +20,14 @@ namespace Tests
             Assert.True(true);
         }
 
-        [Fact]
-        public void BasicCircle() 
+        public SagtFile ejemplo1full(bool toggleComment)
         {
             //STEP 1: Build all our stuff (MFO, ListMeans, AAGS)
 
-            string comment = @"
+            string comment = "";
+            if(toggleComment)
+            {
+                comment = @"
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Users might enter emojis like 😃, 
                 special symbols such as <>&{}[]();@#$%, or quotes 'single' and ""double"" in a description.  
 
@@ -41,7 +43,10 @@ namespace Tests
                 var json = { ""key"": ""value"", ""arr"": [1, 2, 3] };
 
                 Finally, very long lines are a reality: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-            //Error menor detectado: se romperá la app si el usuario llega a escribir '</file__analysis_ssq_comment>' en el comentario, u otros comandos.
+                //Error menor detectado: se romperá la app si el usuario llega a escribir '</file__analysis_ssq_comment>' en el comentario, u otros comandos.
+            }
+
+
 
             ListFacets lf = new ListFacets();
             Facet f1 = new Facet("O", 2);
@@ -88,33 +93,9 @@ namespace Tests
             Analysis_and_G_Study aags = new Analysis_and_G_Study(ldesigns, mfo, differentiation, instrumentation, false);
             aags.SetNameFileDataCreation("testing.sagt");
 
-            SagtFile sagtFile = new SagtFile(mfo, lmeans, aags);
-
-            //STEP 2: Save, Reload, Compare. NOTE: We haven't made fitting .Equal methods, so we're relying on our DataSet methods (not checked, not fully reliable)
-            string firstFile = Path.GetTempFileName();
-            sagtFile.WritingSagtFile(firstFile);
-            SagtFile readSagtFile = SagtFile.ReadingSagtFile(firstFile);
-
-            Assert.Equal(NormalizeXml(sagtFile.GetMultiFacetsObs().MultiFacetObs2DataSet().GetXml()), NormalizeXml(readSagtFile.GetMultiFacetsObs().MultiFacetObs2DataSet().GetXml()));
-
-            //AssertDataSetArraysEqual(sagtFile.GetListMeans().ListMeans2DataSet(), readSagtFile.GetListMeans().ListMeans2DataSet());   
-            //this one directly doesn't work it has the labels juggled, pretty sure it's the dataset function's fault
-
-            //AssertDataSetArraysEqual(sagtFile.GetAnalysis_and_G_Study().Analysis_and_G_Study2ListDataSets(), readSagtFile.GetAnalysis_and_G_Study().Analysis_and_G_Study2ListDataSets());
-            //Found issue: decimal precision is lost when saving? Investigate if this is the case
-            //Expected: ···sq>0.083333333333314386</ssq>\n<msq>0.083333333333314386</msq>···
-            //Actual:   ···sq > 0.0833333333333144 </ ssq >\n < msq > 0.0833333333333144 </ msq >\n < ra···
-
-            //STEP 3: Save again, Compare
-
-            string secondFile = Path.GetTempFileName();
-            readSagtFile.WritingSagtFile(secondFile);
-
-            string firstText = Normalize(File.ReadAllText(firstFile));
-            string secondText = Normalize(File.ReadAllText(secondFile));
-
-            Assert.Equal(firstText, secondText);
+            return new SagtFile(mfo, lmeans, aags);
         }
+
         private static void AssertDataSetArraysEqual(DataSet[] expected, DataSet[] actual)
         {
             Assert.Equal(expected.Length, actual.Length); // ensure same number of DataSets
@@ -167,6 +148,37 @@ namespace Tests
 
             // Trim overall leading/trailing whitespace
             return text.Trim();
+        }
+
+        [Fact]
+        public void BasicCircle() 
+        {
+            SagtFile sagtFile = ejemplo1full(true);
+
+            //STEP 1: Save, Reload, Compare. NOTE: We haven't made fitting .Equal methods, so we're relying on our DataSet methods (not checked, not fully reliable)
+            string firstFile = Path.GetTempFileName();
+            sagtFile.WritingSagtFile(firstFile);
+            SagtFile readSagtFile = SagtFile.ReadingSagtFile(firstFile);
+
+            Assert.Equal(NormalizeXml(sagtFile.GetMultiFacetsObs().MultiFacetObs2DataSet().GetXml()), NormalizeXml(readSagtFile.GetMultiFacetsObs().MultiFacetObs2DataSet().GetXml()));
+
+            //AssertDataSetArraysEqual(sagtFile.GetListMeans().ListMeans2DataSet(), readSagtFile.GetListMeans().ListMeans2DataSet());   
+            //this one directly doesn't work it has the labels juggled, pretty sure it's the dataset function's fault
+
+            //AssertDataSetArraysEqual(sagtFile.GetAnalysis_and_G_Study().Analysis_and_G_Study2ListDataSets(), readSagtFile.GetAnalysis_and_G_Study().Analysis_and_G_Study2ListDataSets());
+            //Found issue: decimal precision is lost when saving? Investigate if this is the case
+            //Expected: ···sq>0.083333333333314386</ssq>\n<msq>0.083333333333314386</msq>···
+            //Actual:   ···sq > 0.0833333333333144 </ ssq >\n < msq > 0.0833333333333144 </ msq >\n < ra···
+
+            //STEP 2: Save again, Compare
+
+            string secondFile = Path.GetTempFileName();
+            readSagtFile.WritingSagtFile(secondFile);
+
+            string firstText = Normalize(File.ReadAllText(firstFile));
+            string secondText = Normalize(File.ReadAllText(secondFile));
+
+            Assert.Equal(firstText, secondText);
         }
 
         //todo: test correct error output of reading broken files
